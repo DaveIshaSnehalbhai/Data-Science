374c7fe87de2cef8c32c589b96f51573fefd48c2
Isha Dave <isdave003@gmail.com>
2025-08-15 15:56:51 +0530

    Done

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os,sys
 import pandas as pd
 import dill
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -41,3 +42,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
